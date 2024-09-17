4b9f7f4be4ca2219bfd5bf2fc59d4d9abb02dc10
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2024-09-17 10:22:00 +0330

    feat: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <div>
+    <iframe src='https://bb.vuecasts.ir' style={{height: '100dvh', width: '100dvw'}} />
+  </div>
+  )
+}
+
+export default App
